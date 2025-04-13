9dc94ecb4ec95eb67c4690da7734233475e6f4be
troykw1 <81531893+troykw1@users.noreply.github.com>
2025-04-13 11:52:54 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// main.js
+
+const Crudapp = require('./Crudapp');
+const Contact = require('./Contact');
+const Csvtopostgres = require('./Csvtopostgres');
+// const Search = require('./Search');
+
+console.log(Crudapp.message);
+console.log(Contact.message);
+console.log(Csvtopostgres.message);
+// console.log(Search.message);
