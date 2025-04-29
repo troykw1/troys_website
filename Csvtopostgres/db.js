aa85e78005acc656ee2f0e92465377f71885e47f
troykw1 <81531893+troykw1@users.noreply.github.com>
2025-04-28 20:44:57 -0700

    Update db.js

--- Csvtopostgres/db.js
+++ Csvtopostgres/db.js
@@ -1,11 +1,11 @@
 const pgp = require('pg-promise')(); // Import pg-promise and create pgp instance [4, 7, 12]
 
 const db = pgp({
-  host: 'localhost',
-  port: 5433,
-  database: 'test',
-  user: 'postgres',
-  password: 'M1n1c00per',
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE
 });
 module.exports = db;
 
